7a2abae69e10586d7a673ca83673d8fb12c231eb
SuchodolskiEdvin <edvinsuchodolskij@gmail.com>
2025-05-10 20:52:54 +0200

    - Create tests for ScoreBoard.updateScore()

--- src/test/java/pl/coconet/scoreboard/ScoreBoardTest.java
+++ src/test/java/pl/coconet/scoreboard/ScoreBoardTest.java
@@ -3,11 +3,14 @@ package pl.coconet.scoreboard;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ScoreBoardTest {
+
+	// ScoreBoard.startGame() TESTS --------------------------------------------------------------------------------
 
 	@Test
 	void shouldStartGameWithInitialZeroScore() {
@@ -50,5 +53,45 @@ public class ScoreBoardTest {
 				board.startGame("France", "Germany")
 		);
 	}
+
+	// ScoreBoard.updateScore() TESTS ------------------------------------------------------------------------------
+
+	@Test
+	void shouldUpdateScoreOfExistingMatch() {
+		ScoreBoard board = new ScoreBoard();
+		board.startGame("Mexico", "Canada");
+		board.updateScore("Mexico", "Canada", 3, 1);
+
+		List<String> summary = board.getSummary();
+		assertEquals(1, summary.size());
+		assertEquals("Mexico 3 - Canada 1", summary.get(0));
+	}
+
+	@Test
+	void shouldThrowWhenUpdatingNonExistentMatch() {
+		ScoreBoard board = new ScoreBoard();
+
+		assertThrows(NoSuchElementException.class, () ->
+				board.updateScore("Mexico", "Canada", 2, 2)
+		);
+	}
+
+	@Test
+	void shouldThrowWhenUpdatingWithNegativeScores() {
+		ScoreBoard board = new ScoreBoard();
+		board.startGame("Mexico", "Canada");
+
+		assertThrows(IllegalArgumentException.class, () ->
+				board.updateScore("Mexico", "Canada", -1, 2)
+		);
+
+		assertThrows(IllegalArgumentException.class, () ->
+				board.updateScore("Mexico", "Canada", 2, -3)
+		);
+	}
+
+	// ScoreBoard.finishGame() TESTS -------------------------------------------------------------------------------
+
+	// ScoreBoard.getSummary() TESTS -------------------------------------------------------------------------------
 
 }
